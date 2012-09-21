970f7bc04f80e4e16bff6ad3dd3befef7d2c2d19
Christian Joudrey <cmallette@gmail.com>
2012-09-20 22:55:22 -0400

    Cleanup test_helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,2 @@
-APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-require File.join(APP_ROOT, 'lib/screenshot_to_imgur')
-
 require 'test/unit'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'screenshot_to_imgur')
