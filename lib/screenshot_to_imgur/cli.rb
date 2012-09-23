5da268770b76b8c2632304fed1aca9e566e17acf
Christian Joudrey <cmallette@gmail.com>
2012-09-23 18:10:14 -0400

    Added .travis.yml

--- lib/screenshot_to_imgur/cli.rb
+++ lib/screenshot_to_imgur/cli.rb
@@ -1,6 +1,15 @@
 module ScreenshotToImgur
   class CLI
     def self.start
+      ScreenshotToImgur.configure do |c|
+        c.path_to_screenshots = '/tmp'
+      end
+
+      callback = Proc.new do |file|
+        p file
+      end
+
+      ScreenshotToImgur::Watcher.new.watch(path: '/tmp', uploader: callback)
     end
   end
 end
