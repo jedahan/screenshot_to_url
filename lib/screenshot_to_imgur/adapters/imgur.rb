bf1856996ddc8d81627a3cb5e1bf2fea6688239e
Christian Joudrey <cmallette@gmail.com>
2012-09-28 21:12:20 -0400

    Added Imgur adapter.

--- lib/screenshot_to_imgur/adapters/imgur.rb
+++ lib/screenshot_to_imgur/adapters/imgur.rb
@@ -1,6 +1,28 @@
+require 'imgur'
+
 module ScreenshotToImgur
   module Adapters
     class Imgur < Adapter
+      attr_reader :api_key
+
+      def initialize(options = {})
+        configure(options)
+      end
+
+      def configure(options)
+        options.each do |key, value|
+          instance_variable_set("@#{key}", value)
+        end
+      end
+
+      def connector
+        @connector ||= ::Imgur::API.new(api_key)
+      end
+
+      def upload_file(file)
+        image_data = connector.upload_file(file)
+        image_data['original_image']
+      end
     end
   end
 end
---
+++ test/screenshot_to_imgur/adapters/imgur_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class ImgurTest < Test::Unit::TestCase
+  include ScreenshotToImgur::Adapters
+
+  def setup
+    @imgur = Imgur.new
+  end
+
+  def test_configure
+    options = { api_key: '1234' }
+
+    @imgur.configure(options)
+
+    assert_equal '1234', @imgur.api_key
+  end
+
+  def test_initialize_with_options
+    options = { api_key: '1234' }
+
+    @imgur = Imgur.new(options)
+
+    assert_equal '1234', @imgur.api_key
+  end
+
+  def test_connector
+    assert_equal ::Imgur::API, @imgur.connector.class
+  end
+
+  def test_connector_options
+    options = { api_key: '1234' }
+
+    @imgur = Imgur.new(options)
+
+    assert_equal '1234', @imgur.connector.instance_variable_get('@api_key')
+  end
+
+  def test_upload_file
+    image_data = { 'original_image' => 'http://example.com' }
+
+    @imgur.connector.expects(:upload_file).once.with('/tmp/test.png').returns(image_data)
+
+    assert_equal 'http://example.com', @imgur.upload_file('/tmp/test.png')
+  end
+end
